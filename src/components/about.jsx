0a068e7960b2f512ea18a0c0a65f1e4f85f06863
Kyle Warren <qarren@gmail.com>
2016-09-27 12:58:59 -0700

    fixups

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,8 +4,8 @@ import css from './about.css';
 
 export default () => (
   <div className={css.about}>
-    <h1>About</h1>
-    <p>Toycast is a podcast for little pieces of software. It was created by <a href="http://kylejwarren">Kyle Warren</a></p>
+    <h1>What?</h1>
+    <p>Toycast is a podcast for little pieces of software. It was created by <a href="http://kylejwarren.com">Kyle Warren</a>.</p>
     <p>Toycast attempts to find joy in code by keeping things small, discoverable, and experimental. Toys will be released episodically and will be mostly open source.</p>
   </div>
 );
